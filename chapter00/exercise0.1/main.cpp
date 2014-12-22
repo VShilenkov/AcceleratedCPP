fdf6fe20943c00897dc8a25846aa07b287d697d0
Vitaliy Shilenkov <vshilenkov@hotmail.com>
2014-12-22 17:24:04 +0200

    Fix

--- chapter00/exercise0.1/main.cpp
+++ chapter00/exercise0.1/main.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /******************************************************************************
  * Project        AcceleratedCPP
  * (c) copyright  2014
@@ -13,41 +12,21 @@
  *    @author     VShilenkov
  *    @date       2014/12/22
  *    @time       17:17:13
- *    @brief      
- */
-
-/******************************************************************************
- * Project        AcceleratedCPP
- * (c) copyright  2014
- * Company        Home
- *                All rights reserved
- * Secrecy Level  PUBLIC
- *****************************************************************************/
-
-/**
- *    @file       main.cpp
- *    @dir        D:\Projects\C++\AcceleratedCPP\chapter00\exercise0.1
- *    @author     VShilenkov
  *    @brief      Упражнение 0.1. Страница 27.
- *    
+ *
  *    Эффективное программирование на C++. Эндрю Кёниг, Барбара Му. 2002.
- *    
+ *
  *    Скомпилируйте и выполните программу, которая выводит текст "Привет, мир!".
+ *
  */
 
-=======
->>>>>>> 3597130556f94d18a8afd2fd0da77c8a4c01163b
 #include <iostream>
 
 int main()
 {
-<<<<<<< HEAD
    setlocale(LC_ALL, "Russian");
 
    std::cout << "Привет, мир!";
 
-=======
-   std::cout << "Hello, world!";
->>>>>>> 3597130556f94d18a8afd2fd0da77c8a4c01163b
    return 0;
 }
