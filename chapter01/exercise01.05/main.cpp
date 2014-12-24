e01b058184eb357b897a5b9c090e7d4c3fbc6803
Vitaliy Shilenkov <vshilenkov@hotmail.com>
2014-12-24 20:18:11 +0200

    chapter01. exercise 05

---
+++ chapter01/exercise01.05/main.cpp
@@ -0,0 +1,46 @@
+/******************************************************************************
+ *       Project: AcceleratedCPP
+ * (c) Copyright: 2014
+ *       Company: Home
+ *                All rights reserved
+ * Secrecy Level: PUBLIC
+ *****************************************************************************/
+
+/**
+ *    @file       main.cpp
+ *    @author     VShilenkov
+ *    @brief      Параграф 01. Упражнение 05. Страница 037.
+ *
+ *    Допустима ли такая программа? Если да, то что она делает? Если нет, то ответьте,
+ *    почему, и предложите допустимый вариант.
+ *
+ *    @see        Эффективное программирование на C++.
+ *    @see        Практическое программирование на примерах.
+ *    @see        Эндрю Кёниг, Барбара Му. 2002.
+ */
+ 
+/**
+ *   Version history:
+ *
+ *   2014-12-24   0.1.0   VShilenkov   Initial
+ */
+
+#include <iostream>
+#include <string>
+
+int main()
+{
+   setlocale(LC_ALL, "Russian");
+   { std::string s = "одна строка";
+   { std::string x = s + ", действительно";
+     std::cout << s << std::endl; //}
+     std::cout << x << std::endl; }
+   }
+   return 0;
+}
+
+
+// main.cpp: In function 'int main()' :
+// main.cpp : 37 : 19 : error : 'x' was not declared in this scope
+// std::cout << x << std::endl;
+//              ^
