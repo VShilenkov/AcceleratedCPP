6fff5345e3da3514c0a3e15974344da8a583bf29
Vitaliy Shilenkov <vshilenkov@hotmail.com>
2014-12-24 16:09:13 +0200

    chapter 01. exercise 04

---
+++ chapter01/exercise01.04/main.cpp
@@ -0,0 +1,41 @@
+/******************************************************************************
+ *       Project: AcceleratedCPP
+ * (c) Copyright: 2014
+ *       Company: Home
+ *                All rights reserved
+ * Secrecy Level: PUBLIC
+ *****************************************************************************/
+
+/**
+ *    @file       main.cpp
+ *    @author     VShilenkov
+ *    @brief      Параграф 01. Упражнение 04. Страница 036.
+ *    
+ *    Что можно сказать об этой программе? Что произойдет, если в третьей строке с
+ *    конца две фигурные скобки }} заменить };}?
+ *
+ *    @see        Эффективное программирование на C++.
+ *    @see        Практическое программирование на примерах.
+ *    @see        Эндрю Кёниг, Барбара Му. 2002.
+ */
+ 
+/**
+ *   Version history:
+ *
+ *   2014-12-24   0.1.0   VShilenkov   Initial
+ */
+
+#include <iostream>
+#include <string>
+
+int main()
+{
+   setlocale(LC_ALL, "Russian");
+
+   { const std::string s = "одна строка";
+     std::cout << s << std::endl;
+   { const std::string s = "другая строка";
+   std::cout << s << std::endl; }; }
+
+   return 0;
+} 
