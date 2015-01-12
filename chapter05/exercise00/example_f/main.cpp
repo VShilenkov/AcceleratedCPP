9802e0aa9bdce8df1704682ed3c4cc6278dc6e09
Vitaliy Shilenkov <vshilenkov@hotmail.com>
2015-01-12 20:46:58 +0200

    chapter 05. exercise 00. example f

---
+++ chapter05/exercise00/example_f/main.cpp
@@ -0,0 +1,86 @@
+/******************************************************************************
+ *       Project: AcceleratedCPP
+ * (c) Copyright: 2015
+ *       Company: Home
+ *                All rights reserved
+ * Secrecy Level: PUBLIC
+ *****************************************************************************/
+
+/**
+ *    @file       main.cpp
+ *    @author     VShilenkov
+ *    @brief      Параграф 05. Упражнение 00. Страница 127.
+ *
+ *    Скомпилируйте, выполните и протестируйте программы, приведенные в этой главе.
+ *    (Пример f. Страница 120)
+ *
+ *    @see        Эффективное программирование на C++.
+ *    @see        Практическое программирование на примерах.
+ *    @see        Эндрю Кёниг, Барбара Му. 2002.
+ */
+ 
+/**
+ *   Version history:
+ *
+ *   2015-01-12   0.1.0   VShilenkov   Initial
+ */
+
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::max;
+using std::string;
+using std::vector;
+
+string::size_type width(const vector<string>& v)
+{
+   string::size_type maxlen = 0;
+   for (vector<string>::size_type i = 0; i != v.size(); ++i)
+   {
+      maxlen = max(maxlen, v[i].size());
+   }
+
+   return maxlen;
+}
+
+vector<string> frame(const vector<string>& v)
+{
+   vector<string> ret;
+   string::size_type maxlen = width(v);
+   string border(maxlen + 4, '*');
+
+   ret.push_back(border);
+
+   for (vector<string>::size_type i = 0; i != v.size(); ++i)
+   {
+      ret.push_back("* " + v[i] + string(maxlen - v[i].size(), ' ') + " *");
+   }
+
+   ret.push_back(border);
+
+   return ret;
+}
+
+int main()
+{
+   string s;
+   vector<string> v;
+   while (cin >> s)
+   {
+      v.push_back(s);
+   }
+
+   vector<string> b = frame(v);
+
+   for (vector<string>::iterator i = b.begin(); i != b.end(); i++)
+   {
+      cout << *i << endl;
+   }
+
+   return 0;
+}
