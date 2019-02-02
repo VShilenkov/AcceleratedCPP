0b8ef2720fe34a375477e7f2aac392f33a9456bb
Vitalii Shylienkov <vshilenkov@hotmail.com>
2019-02-02 22:42:24 +0100

    move toolchains to its own folder

--- cmake/toolchain/gcc_toolchain.cmake
+++ cmake/toolchain/gcc_toolchain.cmake
@@ -4,4 +4,9 @@ set(CMAKE_LINKER          "ld"  CACHE STRING "" FORCE)
 set(CMAKE_C_COMPILER_ID   "GNU" CACHE STRING "" FORCE)
 set(CMAKE_CXX_COMPILER_ID "GNU" CACHE STRING "" FORCE)
 
+if (NOT WIN32)
+    set(CMAKE_EXECUTABLE_SUFFIX_CXX ".out" CACHE STRING "" FORCE)
+    set(CMAKE_EXECUTABLE_SUFFIX_C   ".out" CACHE STRING "" FORCE)
+endif(NOT WIN32)
+
 add_compile_options("SHELL:-Wall -Wpedantic -Wextra")
